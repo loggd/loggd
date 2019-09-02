735397e0f899e1a97d7e207788016359b7ea17b3
Evan Duncan <evan.e.duncan@gmail.com>
2019-09-02 13:07:36 -0500

    Don't reference extension name on image assets

--- app/decorators/entry_decorator.rb
+++ app/decorators/entry_decorator.rb
@@ -3,7 +3,7 @@ class EntryDecorator < ApplicationDecorator
 
   def card_view
     h.content_tag :div, class: 'card' do
-      h.image_tag('paper.png', class: 'img-fluid') +
+      h.image_tag('paper', class: 'img-fluid') +
         h.content_tag(:div, class: 'card-body') do
           h.content_tag(:h5, object.title, class: 'card-title') +
             h.content_tag(:p, "created #{h.distance_of_time_in_words(object.created_at, Time.zone.now)} ago", class: 'card-text text-muted') +
