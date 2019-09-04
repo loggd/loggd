62d90eb4e0bd7d9f4f9657147973e1c54cc13b37
Evan Duncan <evan.e.duncan@gmail.com>
2019-09-04 17:15:48 -0500

    Add Bengali locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || journals_path
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
       add_attrs = [:handle, :email, :password, :password_confirmation, :remember_me]
       devise_parameter_sanitizer.permit :sign_up, keys: add_attrs
       devise_parameter_sanitizer.permit :account_update, keys: add_attrs
+    end
+
+    def switch_locale(&action)
+      locale = params[:locale] || I18n.default_locale
+      I18n.with_locale(locale, &action)
     end
 end
